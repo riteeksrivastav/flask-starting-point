345ce60cdb53ca9ab000100369cda1d83b32ca64
riteek <riteek.s@go-jek.com>
2021-01-10 19:18:34 +0530

    [Riteek] Add support for wsgi

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,10 @@ from flask import Response
 from http import HTTPStatus
 import json
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/ping')
+@application.route('/ping')
 def ping():
     return Response(json.dumps({'ping': 'pong'}),
                 status=HTTPStatus.OK,
                 mimetype='application/json')
-    
-if __name__ == '__main__':
-    app.run()
