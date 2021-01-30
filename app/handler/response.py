6eac4f61d399f79290b640adb682587ed0026ad4
riteek <riteek.s@go-jek.com>
2021-01-30 17:14:11 +0530

    [Riteek] Refactor responses

---
+++ app/handler/response.py
@@ -0,0 +1,20 @@
+from http import HTTPStatus
+from flask import Response
+import json
+
+def success(data):
+    return Response(json.dumps({'message': data}),
+                    status=HTTPStatus.OK,
+                    mimetype='application/json')
+
+
+def invalid_request(error):
+    return Response(json.dumps({'error': error}),
+                    status=HTTPStatus.BAD_REQUEST,
+                    mimetype='application/json')
+
+
+def internal_server_error(error):
+    return Response(json.dumps({'error': None}),
+                    status=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    mimetype='application/json')
--- app/handler/user.py
+++ app/handler/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response, request
 from http import HTTPStatus
+import app.handler.response as resp
 import app.user.service as user_service
 import app.user.exceptions as exceptions
 import json
@@ -16,24 +17,16 @@ def create_user():
     age = request.form.get('age')
     if name == "" or age == "":
         logger.error('name or age should not be empty')
-        return Response(json.dumps({'error': 'name or age should not be empty'.format(name)}),
-                status=HTTPStatus.BAD_REQUEST,
-                mimetype='application/json')
+        return resp.invalid_request('name or age should not be empty'.format(name))
 
     try:
         user_service.create(name, age)
-        logger.info('user with name {} has been successfully registers'.format(name))
-        return Response(json.dumps({'message': 'User {} has been successfully registered'.format(name)}),
-                status=HTTPStatus.OK,
-                mimetype='application/json')
+        logger.info('user with name {} has been successfully registered'.format(name))
+        return resp.success('User {} has been successfully registered'.format(name))
     except exceptions.UserAlreadyExists:
         logger.error('user with name {} already registerd'.format(name))
-        return Response(json.dumps({'error': 'User {} is already registered'.format(name)}),
-                status=HTTPStatus.BAD_REQUEST,
-                mimetype='application/json')
+        return resp.invalid_request('User {} is already registered'.format(name))
     except Exception as e:
         logger.error('unable to register user with name {}, error: {}'.format(name, e))
-        return Response(json.dumps({'error': 'something went wrong, please try after sometime'}),
-                status=HTTPStatus.INTERNAL_SERVER_ERROR,
-                mimetype='application/json')
+        return resp.internal_server_error('something went wrong, please try after sometime')
     
